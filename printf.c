804b3544ec4e149f751b8423e109e57d6d9c618c
victoria igbojekwe <toriaokechukwu.com>
2023-03-28 21:29:54 -0700

    fix betty error

--- printf.c
+++ printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * specifier-check - check the validty of the printf secifier
+ * specifer_check - check the validty of the printf secifier
  * and call the related function.
- * @spec: Specifer to be checked to determine return function.
+ * @spec: Specifier to be checked to determine return function.
  * Return: pinter to a function that returns( int) and takes a
  * (va_list) as arguments.
 */
@@ -12,15 +12,17 @@ specifier_handler specifer_check(char spec)
 	{
 		int i;
 
-		handler int_handler = {'i',handle_int};
+		handler int_handler = {'i', handle_int};
 		handler str_handler = {'s', handle_str};
 		handler char_handler = {'c', handle_char};
 		handler cent_handler = {'%', handle_cent};
 		handler dec_handler = {'d', handle_dec};
 		handler null_handler = {'\0', NULL};
 
-		handler handlers[] = {char_handler, str_handler, cent_handler, dec_handler, int_handler, null_handler};
-		for(i = 0; handlers[i].c != '\0'; i++)
+		handler handlers[] = {char_handler, str_handler,
+		       cent_handler, dec_handler, int_handler, null_handler};
+
+		for (i = 0; handlers[i].c != '\0'; i++)
 		{
 			if (handlers[i].c == spec)
 			{
@@ -36,10 +38,11 @@ specifier_handler specifer_check(char spec)
  * @input: string to be printed.
  * Return: no of chars printed.
 */
-int _printf (const char *input, ...)
+int _printf(const char *input, ...)
 {
 	int count = 0, i = 0;
 	va_list args;
+
 	va_start(args, input);
 	specifier_handler handler;
 
@@ -59,5 +62,5 @@ int _printf (const char *input, ...)
 		}
 	}
 	va_end(args);
-	return (count);
+return (count);
 }
